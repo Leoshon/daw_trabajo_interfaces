744916de80765e12b4616e08adaf7ff006b47d31
Leoshon <131702139+Leoshon@users.noreply.github.com>
2024-01-31 15:24:10 +0100

    pages created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Productos from './Productos';
+import Contact from './Contact';
+
 
 function App() {
+  let component
+  switch (window.location.pathname) {
+    case "/home":
+      component = <Home/>;
+      break;
+    case "/about":
+      component = <About/>;
+      break;
+    case "/products":
+      component = <Productos/>;
+      break;
+    case "/contact":
+      component = <Contact/>;
+      break;
+  
+    default:
+      break;
+  }
+
   return (
-    <div className="App">
+    <>
     <Navbar />
+    <div className="container">
+    {component}
     </div>
+    </>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,14 +3,25 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav href="/" className='nav'>
-      <a href="/" className='site-title'>Inicio</a>
+      <a href="/home" className='site-title'>Inicio</a>
       <ul className='nav-links'>
-        <li><a href="/about">Quines somos</a></li>
-        <li><a href="/products">Productos</a></li>
-        <li><a href="/contact">Contacta</a></li>
+        <CustomLink href="/about">Quienes somos</CustomLink>
+        <CustomLink href="/products">Productos</CustomLink>
+        <CustomLink href="/contact">Contacto</CustomLink>
+        
         </ul>
     </nav>
   )
+}
+function CustomLink({href,children, ...props}){
+    const path = window.location.pathname
+    return (
+        <li className={path === href ? "active":""}>
+            <a href={href} {...props}>
+                {children}
+            </a>
+        </li>
+    )
 }
 
 export default Navbar
