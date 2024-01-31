30466f5166ed75f73d81f4d596e77c6d4aad7efd
Leoshon <131702139+Leoshon@users.noreply.github.com>
2024-01-31 18:49:23 +0100

    css configs

--- src/Contact.js
+++ src/Contact.js
@@ -1,37 +1,41 @@
 import React from "react";
+import { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import contactImg from "./assets/img/contact-img.svg";
 
 const Contact = () => {
+  const [buttonText, setButtonText] = useState("Enviar");
+  const setText = () => {
+    setButtonText("Enviando...");
+  };
   return (
     <section id="contact" className="contact">
-      <h1>Contacto</h1>
       <Container>
         <Row className="align-items-center">
           <Col md={6} sm={4}>
             <img src={contactImg} alt="contact" />
           </Col>
           <Col md={6}>
-            <h2>Contact Me</h2>
+            <h2>Contacta</h2>
             <form>
               <Row>
                 <Col md={6}>
-                  <input type="text" placeholder="First Name" />
+                  <input type="text" placeholder="Nombre" />
                 </Col>
                 <Col md={6}>
-                  <input type="text" placeholder="Last Name" />
+                  <input type="text" placeholder="Apellido" />
                 </Col>
                 <Col md={6}>
                   <input type="email" placeholder="Email" />
                 </Col>
                 <Col md={6}>
-                  <input type="text" placeholder="Phone" />
+                  <input type="text" placeholder="Teléfono" />
                 </Col>
                 <Col md={12}>
-                  <textarea placeholder="Message"></textarea>
+                  <textarea placeholder="Mensaje"></textarea>
                 </Col>
                 <Col md={12}>
-                  <button type="button">Enviar</button>
+                  <button type="button" onClick={setText} >{buttonText}</button>
                 </Col>
               </Row>
             </form>
